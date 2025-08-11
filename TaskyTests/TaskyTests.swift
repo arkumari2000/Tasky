3868911f654b2cc42fcee2820a1bce109f43adaf
Archana Kumari <arkumari2000@gmail.com>
2025-08-11 09:08:06 +0530

    Initial Commit

---
+++ TaskyTests/TaskyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskyTests.swift
+//  TaskyTests
+//
+//  Created by Archana Kumari on 11/08/25.
+//
+
+import Testing
+@testable import Tasky
+
+struct TaskyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
