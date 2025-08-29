06a56483c26f43022361e5dbf86c201040632653
Rishabh@831 <rishabhbhati831@gmail.com>
2025-08-29 19:05:40 +0530

    Comments resolved

--- Tasky/Custom Views/Buttons/TaskyAddkButton.swift
+++ Tasky/Custom Views/Buttons/TaskyAddkButton.swift
@@ -7,31 +7,27 @@
 
 import UIKit
 
-
-class TaskyAddButton:UIButton{
+class TaskyAddButton:UIButton {
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configure()
+        
     }
-   
+    
     init(title:String) {
         super.init(frame: .zero)
-        
         self.setTitle(title, for: .normal)
-        self.setTitleColor(.orange, for: .normal)
-        self.setImage(UIImage(systemName: "plus.circle.fill"), for: .normal)
-        
-        
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    func configure(){
-        layer.cornerRadius = 2
-        
+    func configure() {
+        layer.cornerRadius=2
         translatesAutoresizingMaskIntoConstraints=false
-        setImage(UIImage(systemName: "plus.circle.fill"), for: .normal)
+        self.setTitleColor(.orange, for: .normal)
+        self.setImage(UIImage(systemName: "plus.circle.fill"), for: .normal)
         
     }
 }
